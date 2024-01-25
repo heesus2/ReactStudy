1e72f222b376e0767ac5446443d7b4e084e47d9a
heesus2 <likeheesu@gmail.com>
2024-01-25 16:58:41 +0900

    reactStudyStart

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          안녕하세요
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          소히바보
         </a>
       </header>
     </div>
