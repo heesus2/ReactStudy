7a71af6aedb57b4e257afc414264aac58334479c
heesus2 <likeheesu@gmail.com>
2024-01-28 18:52:05 +0900

    component 공부

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집','파이썬 독학']);
-  let [따봉, 따봉변경] = useState(0);
-  let posts = '강남 고기 맛집'
+  const [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  const [따봉, 따봉변경] = useState(0);
+  const posts = '강남 고기 맛집'
 
   // function 제목바꾸기(){
   //  var newArray = [...글제목];
@@ -15,7 +15,7 @@ function App() {
   //  글제목변경( newArray );
   // }
 
-  function 정렬하기(){
+  function 정렬하기() {
     var newArray = [...글제목];
     newArray.sort();
     글제목변경(newArray);
@@ -27,20 +27,34 @@ function App() {
         <div>개발 Blog </div>
       </div>
       {/* <button onClick={ 제목바꾸기 }>버튼</button> */}
-      <button onClick={ 정렬하기 }>정렬하기</button>
+      <button onClick={정렬하기}>정렬하기</button>
       <div className='list'>
-        <h3>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h3>
+        <h3>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
-        <hr/>
-        <h3>{ 글제목[1] }</h3>
+        <hr />
+        <h3>{글제목[1]}</h3>
         <p>2월 18일 발행</p>
-        <hr/>
-        <h3>{ 글제목[2] }</h3>
+        <hr />
+        <h3>{글제목[2]}</h3>
         <p>2월 19일 발행</p>
-        <hr/>
+        <hr />
       </div>
+
+      <Modal />
+
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className='modal'>
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+    
+  )
 }
 
 export default App;
