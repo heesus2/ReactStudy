0fd764646a0e4b0672691c4be03ca673eb9773f6
heesus2 <likeheesu@gmail.com>
2024-01-28 17:48:25 +0900

    state 공부

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
+/* eslint-disable*/ //-> 작성하면 노란글씨 안뜸
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집','파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
+  let posts = '강남 고기 맛집'
+
+  // function 제목바꾸기(){
+  //  var newArray = [...글제목];
+  //  newArray[0] = '여자 코트 추천';
+  //  글제목변경( newArray );
+  // }
+
+  function 정렬하기(){
+    var newArray = [...글제목];
+    newArray.sort();
+    글제목변경(newArray);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕하세요
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          소히바보
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog </div>
+      </div>
+      {/* <button onClick={ 제목바꾸기 }>버튼</button> */}
+      <button onClick={ 정렬하기 }>정렬하기</button>
+      <div className='list'>
+        <h3>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+        <h3>{ 글제목[1] }</h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+        <h3>{ 글제목[2] }</h3>
+        <p>2월 19일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
